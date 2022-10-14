50211ba0d2c29dff5d5fcb575e29ebe37f88ffaa
YunHo Choi <svs101@dgu.ac.kr>
2022-10-14 23:48:06 +0900

    Feat: JWT 검증 구현(#6)

--- src/main/java/com/curady/apigatewayservice/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/curady/apigatewayservice/filter/JwtAuthenticationFilter.java
@@ -9,7 +9,9 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 
 @Component
 public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
@@ -22,28 +24,32 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
     }
 
     public static class Config {
-        // application.yml 파일에서 지정한 filer의 Argument값을 받는 부분
     }
 
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            if (!exchange.getRequest().getHeaders().containsKey("X-AUTH-TOKEN")) {
-                ServerHttpResponse response = exchange.getResponse();
-                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            final List<String> apiEndpoints = List.of("/auth");
 
-                return response.setComplete();
+            Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                    .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+            if (!isApiSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey("X-AUTH-TOKEN")) {
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+                    return response.setComplete();
+                }
+                String token = exchange.getRequest().getHeaders().get("X-AUTH-TOKEN").get(0);
+                Map<String, Object> userInfo = jwtUtil.getClaims(token);
+
+                addAuthorizationHeaders(exchange.getRequest(), userInfo);
             }
-            String token = exchange.getRequest().getHeaders().get("X-AUTH-TOKEN").get(0);
-            Map<String, Object> userInfo = jwtUtil.getClaims(token);   // 파싱된 토큰의 claim을 추출해 아이디 값을 가져온다.
-
-            addAuthorizationHeaders(exchange.getRequest(), userInfo);
-
             return chain.filter(exchange);
         };
     }
 
-    // 성공적으로 검증이 되었기 때문에 인증된 헤더로 요청을 변경해준다. 서비스는 해당 헤더에서 아이디를 가져와 사용한다.
     private void addAuthorizationHeaders(ServerHttpRequest request, Map<String, Object> userInfo) {
         request.mutate()
                 .header("X-Authorization-Id", userInfo.get("sub").toString())
